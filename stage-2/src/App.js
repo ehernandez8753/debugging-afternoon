d9f6b61d2e175f03f69915b8ef57b4444ab5414c
Esteban Hernandez <contact@mksmdesign.com>
2019-06-25 15:31:03 -0600

    Fixed bugs from stage-2

--- stage-2/src/App.js
+++ stage-2/src/App.js
@@ -5,7 +5,7 @@ import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
 import NavBar from "./Components/NavBar/NavBar";
 
 class App extends Component {
-  constuctor(props) {
+  constructor(props) {
     super(props);
     this.state = {
       products: [],
@@ -21,17 +21,20 @@ class App extends Component {
       .get("https://practiceapi.devmountain.com/products/")
       .then(response => {
         this.setState({
-          products: response
+          products: response.data
         });
       });
   }
   addToCart(item) {
+    let newCart = [];
+    newCart = [...this.state.cart];
+    newCart.push(item);
     this.setState({
-      cart: item
+      cart: newCart
     });
   }
   removeFromCart(index) {
-    let cartCopy = this.state.products.slice();
+    let cartCopy = [...this.state.cart];
     cartCopy.splice(index, 1);
     this.setState({
       cart: cartCopy
